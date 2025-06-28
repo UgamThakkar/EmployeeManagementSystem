3969dc7ea2351325273cc62d1d708aec3f93f357
Ugam Thakkar <thakkarugam@gmail.com>
2025-06-28 07:45:34 -0700

    feat: add getEmployeeById service, controller API, and not-found exception handling

--- src/main/java/javafullstack/ems_backend/service/EmployeeService.java
+++ src/main/java/javafullstack/ems_backend/service/EmployeeService.java
@@ -4,4 +4,6 @@ import javafullstack.ems_backend.dto.EmployeeDto;
 
 public interface EmployeeService {
     EmployeeDto createEmployee(EmployeeDto employeeDto);
+
+    EmployeeDto getEmployeeById(Long employeeId);
 }
--- src/main/java/javafullstack/ems_backend/service/impl/EmployeeServiceImpl.java
+++ src/main/java/javafullstack/ems_backend/service/impl/EmployeeServiceImpl.java
@@ -8,6 +8,8 @@ import javafullstack.ems_backend.service.EmployeeService;
 import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.lang.module.ResolutionException;
+
 @Service
 @AllArgsConstructor
 public class EmployeeServiceImpl implements EmployeeService {
@@ -19,5 +21,13 @@ public class EmployeeServiceImpl implements EmployeeService {
         Employee employee = EmployeeMapper.mapToEmployee(employeeDto);
         Employee savedEmployee = employeeRepository.save(employee);
         return EmployeeMapper.mapToEmployeeDto(savedEmployee);
+    }
+
+    @Override
+    public EmployeeDto getEmployeeById(Long employeeId) {
+        Employee employee = employeeRepository.findById(employeeId)
+                .orElseThrow(()->
+                        new ResolutionException("Employee with this given ID does not exist : "+ employeeId));
+        return EmployeeMapper.mapToEmployeeDto(employee);
     }
 }
